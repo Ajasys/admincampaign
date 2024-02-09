13f2b39753a84b7695fb307881b0c864692d30f6
{--JENISH--}|-|-|-| <jenishshekhada33@gmail.com>
2024-02-09 10:58:21 +0530

    Bot New Degien

--- app/Views/bot.php
+++ app/Views/bot.php
@@ -1,6 +1,14 @@
 <?= $this->include('partials/header') ?>
 <?= $this->include('partials/sidebar') ?>
 <style>
+    body {
+        background-color: whitesmoke;
+    }
+
+    .icon-box {
+        color: red !important;
+    }
+
     .icon-box:hover {
         color: black !important;
     }
@@ -27,29 +35,112 @@
     .card-border-color {
         border-color: #FAFAFA;
     }
+
 </style>
 
 <div class="main-dashbord p-2">
     <div class="p-1">
-        <div class="container-fluid p-0 mb-3">
-            <div class="bg-white px-3 py-1">
+        <!-- <div class="container-fluid p-0 mb-3">
+            <div class="px-3 py-1">
                 <div class="d-flex justify-content-between align-items-center flex-wrap w-100">
                     <div class="title-1">
                         <i class="fa-brands fa-bots fs-1"></i>
                     </div>
                     <div class="title-side-icons">
-                        <button class="btn-primary-rounded add" type="button" data-bs-toggle="modal"
-                            data-bs-target="#bot_crate" aria-controls="bot_crate">
+                        <button class="btn-primary-rounded add" type="button" data-bs-toggle="modal" data-bs-target="#bot_crate" aria-controls="bot_crate">
                             <i class="bi bi-plus"></i>
                         </button>
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
 
-        <div class="container-fluid p-0">
-            <div class="bot_list bg-white px-3 py-3 d-flex flex-wrap">
+        <div class="container-fluid p-4">
+            <div class="col-12 bg-white px-3 p-2 d-flex flex-wrap rounded-4 shadow justify-content-between align-items-center">
+                <div class="col-6 p-2 d-flex flex-wrap justify-content-start align-items-center">
+                    <div class="col-1 d-flex justify-content-start align-items-center">
+                        <div class="title-1">
+                            <i class="fa-brands fa-bots fs-1"></i>
+                        </div>
+                    </div>
+                    <div class="col-6 d-flex justify-content-start align-items-center">
+                        <select class="form-select border border-white" aria-label="Default select example">
+                            <option selected>All</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-6 p-2 d-flex flex-wrap justify-content-end align-items-center">
+                    <div class="col-7 d-flex justify-content-end align-items-center">
+                        <div class="input-group">
+                            <input type="text" class="form-control border border-white" placeholder="Search here for bot" aria-label="Recipient's username" aria-describedby="button-addon2">
+                            <button class="btn btn-secondery border border-white bg-white border-start-0" type="button" id="button-addon2">Q</button>
+                        </div>
+                    </div>
+                    <div class="col-1 d-flex justify-content-end align-items-center">
+                        <button class="btn-primary-rounded add" type="button" data-bs-toggle="modal" data-bs-target="#bot_crate" aria-controls="bot_crate">
+                            <i class="bi bi-plus"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="bot_list bg-white-50 px-3 py-3 my-5 d-flex flex-wrap">
                 <!-- card start -->
+                <!--new card-->
+                <div class="col-3 p-2">
+                    <div class="card mb-3 rounded-4 shadow bot-card">
+                        <div class="row g-0 p-2">
+                            <div class="d-flex align-items-center">
+                                <div class="col-md-4 my-3 text-center bot-name-tab">
+                                    <div class="p-2 my-3">
+                                        <img src="<?= base_url('') ?>assets/images/bot_img/bot-1.png" class="img-fluid rounded-start mb-1" width="40px">
+                                        <p class="card-text mt-2"><small class="text-body-secondary fs-6 fw-bold">Bot Name</small></p>
+                                    </div>
+                                </div>
+                                <div class="border h-100"></div>
+                                <div class="col-md-8 my-2 px-2 bot-data-tab">
+                                    <div class="card-body d-flex flex-wrap py-1 px-1 justify-content-between my-2">
+                                        <div class="border border-1 rounded-3 d-inline w-auto p-1 px-3 py-2 icon-box text-muted" data-toggle="tooltip"  data-placement="top" title="Setup">
+                                            <a href="#" class="text-muted">
+                                                <i class="fa-solid fa-screwdriver-wrench fa-lg"></i>
+                                            </a>
+                                        </div>
+                                        <div class="border rounded-3 d-inline w-auto p-1 px-3 py-2 icon-box text-muted" data-toggle="tooltip" data-placement="top" title="Trigger">
+                                            <a href="#" class="text-muted">
+                                                <i class="fa-solid fa-bell fa-lg"></i>
+                                            </a>
+                                        </div>
+                                        <div class="border rounded-3 d-inline w-auto p-1 px-3 py-2 icon-box text-muted" data-toggle="tooltip" data-placement="top" title="Bot Chats">
+                                            <a href="#" class="text-muted">
+                                                <i class="fa-solid fa-comment fa-lg"></i>
+                                            </a>
+                                        </div>
+                                        <div class="border rounded-3 shadow-sm bg-body-tertiary d-inline w-auto p-1 px-3 py-2 icon-box text-muted" data-toggle="tooltip" data-placement="top" title="Setting">
+                                            <a href="#" class="text-muted">
+                                                <i class="fa-solid fa-gear fa-lg"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="card-body d-flex flex-wrap py-1 px-1 justify-content-between align-items-center mt-4">
+                                        <div class="form-check form-switch ps-0 d-flex align-items-center">
+                                            <label class="switch_toggle_primary">
+                                                <input class="toggle-checkbox" type="checkbox" id="">
+                                                <span class="check_input_primary round"></span>
+                                            </label>
+                                            <p class="mx-2 fw-medium">Active</p>
+                                        </div>
+                                        <div class="border rounded d-inline w-auto p-1 px-2 icon-box2 text-muted" data-toggle="tooltip" data-placement="top" data-bs-placement="right"  title="Delete">
+                                            <i class="fa-solid fa-trash"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--new card end-->
                 <!-- <div class="col-3 p-2">
                     <div class="card mb-3">
                         <div class="row g-0 p-2">
@@ -125,20 +216,16 @@
             </div>
             <div class="modal-body d-flex justify-content-center flex-wrap position-relative ">
                 <div class="col-12">
-                    <div class="w-100 col-11 p-3 d-flex flex-wrap align-items-center my-2 border rounded-3 modal-card"
-                        data-bot_type="1">
-                        <div style="width:50px;height:50px;"
-                            class="border rounded-circle d-flex justify-content-center align-items-center">
+                    <div class="w-100 col-11 p-3 d-flex flex-wrap align-items-center my-2 border rounded-3 modal-card" data-bot_type="1">
+                        <div style="width:50px;height:50px;" class="border rounded-circle d-flex justify-content-center align-items-center">
                             <i class="bi bi-funnel fs-2"></i>
                         </div>
                         <div class="col px-3">
                             <p class="fs-6 text-dark fs-semibold">Get Lead By Chat Bots</p>
                         </div>
                     </div>
-                    <div class="w-100 col-11 p-3 d-flex flex-wrap align-items-center my-2 border rounded-3 modal-card"
-                        data-bot_type="2">
-                        <div style="width:50px;height:50px;"
-                            class="border rounded-circle d-flex justify-content-center align-items-center">
+                    <div class="w-100 col-11 p-3 d-flex flex-wrap align-items-center my-2 border rounded-3 modal-card" data-bot_type="2">
+                        <div style="width:50px;height:50px;" class="border rounded-circle d-flex justify-content-center align-items-center">
                             <i class="bi bi-headset fs-2"></i>
                         </div>
                         <div class="col px-3">
@@ -151,15 +238,13 @@
         <div class="modal-content bot_name_modal">
             <div class="modal-header">
                 <h5 class="modal-title" id="botfirstmodal">Create New Bot</h5>
-                <button type="button" class="close border-none close-modal" data-dismiss="modal" aria-label="Close"
-                    data-bs-toggle="modal">
+                <button type="button" class="close border-none close-modal" data-dismiss="modal" aria-label="Close" data-bs-toggle="modal">
                     <i class="fa-solid fa-xmark"></i>
                 </button>
             </div>
             <div class="modal-body">
                 <label for="botNameInput" class="form-label">Bot Name :</label>
-                <input type="text" id="bot_name" name="bot_name" class="form-control m-auto"
-                    placeholder="name for your bot">
+                <input type="text" id="bot_name" name="bot_name" class="form-control m-auto" placeholder="name for your bot">
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-primary m-auto bot_create_btn" data-bot_type="0">Create</button>
@@ -203,7 +288,7 @@
 
 
 <script>
-    $(function () {
+    $(function() {
         $('[data-toggle="tooltip"]').tooltip()
     })
     // $('.next-card').slideUp();
@@ -221,12 +306,12 @@
     //     $('#first_bot').modal('show').slideDown(1000);
     // });
 
-    $('.add').on('click', function () {
+    $('.add').on('click', function() {
         $('.bot_name_modal').hide();
         $('.bot_type_modal').show();
     });
     $('.bot_name_modal').hide();
-    $('body').on('click', '.modal-card', function () {
+    $('body').on('click', '.modal-card', function() {
         $('.bot_type_modal').slideUp(300);
         $('.bot_type_modal').hide();
         $('.bot_name_modal').slideDown(600);
@@ -241,10 +326,10 @@
             data: {
                 'table': '<?= getMasterUsername2() ?>_bot',
             },
-            beforeSend: function (f) {
+            beforeSend: function(f) {
                 $('.loader').show();
             },
-            success: function (res) {
+            success: function(res) {
                 $('.loader').hide();
                 if (res != '') {
                     $('.bot_list').html(res);
@@ -256,7 +341,7 @@
 
     list_data();
 
-    $('body').on('click', '.bot_create_btn', function (e) {
+    $('body').on('click', '.bot_create_btn', function(e) {
         e.preventDefault();
         var bot_type = $(this).attr('data-bot_type');
         var bot_name = $('#bot_name').val();
@@ -271,10 +356,10 @@
                     'action': 'bot_insert',
                     'table': '<?= getMasterUsername2() ?>_bot',
                 },
-                beforeSend: function (f) {
+                beforeSend: function(f) {
                     $('.loader').show();
                 },
-                success: function (res) {
+                success: function(res) {
                     $('.loader').hide();
                     list_data();
                     $('#bot_name_model').modal('hide');
@@ -297,42 +382,42 @@
         var record_text = "Are you sure you want to delete this Bot?";
         var table = '<?php echo getMasterUsername2(); ?>_bot';
         Swal.fire({
-                title: 'Delete Bot?',
-                text: record_text,
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonText: 'CONFIRM',
-                cancelButtonText: 'CANCEL',
-                cancelButtonColor: '#6e7881',
-                confirmButtonColor: '#dd3333',
-                reverseButtons: true
+            title: 'Delete Bot?',
+            text: record_text,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'CONFIRM',
+            cancelButtonText: 'CANCEL',
+            cancelButtonColor: '#6e7881',
+            confirmButtonColor: '#dd3333',
+            reverseButtons: true
         }).then(function(result) {
-                if (result.value) {
-                    $.ajax({
-                        method: "post",
-                        url: "<?= site_url('bot_delete_data'); ?>",
-                        data: {
-                            action: 'delete',
-                            id: delete_id,
-                            table: table,
-                            bot: '',
-                        },
-                        success: function(data) {
-                            iziToast.error({
-                                    title: 'Deleted Successfully'
-                            });
-                            // $(".close_btn").trigger("click");
-                            list_data();
-                        }
-                    });
-                }
+            if (result.value) {
+                $.ajax({
+                    method: "post",
+                    url: "<?= site_url('bot_delete_data'); ?>",
+                    data: {
+                        action: 'delete',
+                        id: delete_id,
+                        table: table,
+                        bot: '',
+                    },
+                    success: function(data) {
+                        iziToast.error({
+                            title: 'Deleted Successfully'
+                        });
+                        // $(".close_btn").trigger("click");
+                        list_data();
+                    }
+                });
+            }
         });
 
     });
 
-    $('body').on('change','.bot_active',function() {
+    $('body').on('change', '.bot_active', function() {
         var update_id = $(this).data('update_id');
-        if($(this).is(':checked')) {
+        if ($(this).is(':checked')) {
             var status = 1;
             var massage = 'Bot activated!';
         } else {
@@ -340,7 +425,7 @@
             var massage = 'Now Bot is inactivate';
         }
 
-        if(update_id != '') {
+        if (update_id != '') {
             var table = '<?php echo getMasterUsername2(); ?>_bot';
             var second_table = '<?php echo getMasterUsername2(); ?>_bot_setup';
             $.ajax({
@@ -355,17 +440,17 @@
                     second_table: second_table,
                 },
                 success: function(data) {
-                    if(data == 'empty') {
+                    if (data == 'empty') {
                         iziToast.error({
                             title: 'Please add bot setup first for this bot!',
                         });
 
-                        setTimeout(function(){
-                            window.location.href = "<?= base_url(); ?>bot_setup?bot_id="+update_id;
+                        setTimeout(function() {
+                            window.location.href = "<?= base_url(); ?>bot_setup?bot_id=" + update_id;
                         }, 2000);
                     } else {
                         iziToast.success({
-                                title: massage,
+                            title: massage,
                         });
                     }
                     list_data();
@@ -373,5 +458,4 @@
             });
         }
     });
-
 </script>
