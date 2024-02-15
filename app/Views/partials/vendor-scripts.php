9c7c2db2b9a203d4cea946e0873726206ea27026
Aayush dave <aayushdave2312@gmail.com>
2024-02-15 11:30:55 +0530

    add slider link css and js

--- app/Views/partials/vendor-scripts.php
+++ app/Views/partials/vendor-scripts.php
@@ -20,6 +20,9 @@
 <!-- country name and code -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.13/css/intlTelInput.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.13/js/intlTelInput-jquery.min.js"></script>
+<!-- swiper-slider -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
 
 <script>
