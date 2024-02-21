178e841228266282c9f51b15f6f8e36ed82bca5d
Foram Savaliya <foramsavaliya98@gmail.com>
2024-02-21 15:12:20 +0530

    linkin code

--- app/Controllers/LinkedinController.php
+++ app/Controllers/LinkedinController.php
@@ -3,10 +3,9 @@
 namespace App\Controllers;
 
 use App\Models\MasterInformationModel;
-use Config\Database;
-use DateInterval;
-use DateTime;
-use DateTimeZone;
+use Exception;
+use League\OAuth2\Client\Provider\LinkedIn;
+
 
 class LinkedinController extends BaseController
 {
@@ -26,6 +25,51 @@ class LinkedinController extends BaseController
 
     public function add_linkedin_connection()
     {
-        
+        $client_secret = $this->request->getPost("client_secret");
+        $client_id = $this->request->getPost("client_id");
+        $access_token = $this->request->getPost("access_token");
+        $provider = new LinkedIn([
+            'clientId' => $client_id,
+            'clientSecret' => $client_secret,
+        ]);
+
+        $provider->setAccessToken(['access_token' => $access_token]);
+
+        // Make a request to LinkedIn's API
+        try {
+            // Replace 'me' with the endpoint you want to access
+            $response = $provider->get('me');
+
+            // Output the response
+            var_dump($response);
+        } catch (Exception $e) {
+            // Handle any errors
+            echo 'Error: ' . $e->getMessage();
+        }
+
+        // if (!isset($_GET['code'])) {
+        //     // If we don't have an authorization code, then get one
+        //     $authUrl = $provider->getAuthorizationUrl();
+        //     $_SESSION['oauth2state'] = $provider->getState();
+        //     header('Location: ' . $authUrl);
+        //     exit;
+        // } elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
+        //     // Check given state against previously stored one to mitigate CSRF attack
+        //     unset($_SESSION['oauth2state']);
+        //     exit('Invalid state');
+        // } else {
+        //     // Try to get an access token using the authorization code grant
+        //     $token = $provider->getAccessToken('authorization_code', [
+        //         'code' => $_GET['code'],
+        //     ]);
+
+        //     // Optional: Use the token to fetch user information
+        //     try {
+        //         $user = $provider->getResourceOwner($token);
+        //         print_r($user->toArray());
+        //     } catch (Exception $e) {
+        //         exit('Failed to get user details: ' . $e->getMessage());
+        //     }
+        // }
     }
 }
